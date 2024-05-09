69f4b9a969555a393177ed251d93c653326a62c0
jadavPintu28 <jadavpintu43@gmail.com>
2024-05-09 10:07:43 +0530

    var modifed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const qrText = document.getElementById('qr-text');
 const sizes = document.getElementById('sizes');
 const generateBtn = document.getElementById('generateBtn');
 const downloadBtn = document.getElementById('downloadBtn');
-const qrContainer = document.querySelector('.qr-body');
+const qrImg = document.querySelector('.qr-body');
 
 let size = sizes.value;
 generateBtn.addEventListener('click',(e)=>{
